c6f977d0a823c797a5c538ea5ca1f5c4600c9a2a
AS <albrecht.stephanie@outlook.fr>
2019-10-25 14:10:23 +0200

    bouton RAZ avec champ rempli

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+function raz(){ // remplace value indiqué dans le html
+    document.getElementById("lastname").value = "";
+    document.getElementById("firstname").value = "";
+    document.getElementById("city").value = "";
+}
